1ce9858f1e5a254d7a764c75211141c16d28e700
Rohan-Peddamallu <rohanpeddamallu@gmail.com>
2024-11-25 08:44:05 -0600

    Started on startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,26 @@
 import Image from "next/image";
+import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const posts = [
+    {
+      createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Adrian" },
+      _id: 1,
+      description: "This is a description",
+      image: "https://via.placeholder.com/150",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -11,6 +31,22 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit ideas, Vote on Pithces, and Get Noticed in Virtual Competitions
         </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
